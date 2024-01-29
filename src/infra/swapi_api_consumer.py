5f357bb6c36c4afbde5e7580a1b44c2c58d0f078
Luiz Felipe <luiz.siqueira695@gmail.com>
2024-01-29 15:55:01 -0300

    feat: consertando erro da implementação do teste de error da swapi_api-consumer

--- src/infra/swapi_api_consumer.py
+++ src/infra/swapi_api_consumer.py
@@ -1,17 +1,24 @@
 import requests
 from requests import Request
-from typing import Type
+from typing import Type, Tuple, Dict
 from collections import namedtuple
 from src.errors.http_request_error import HttpRequestError
 
 class SwapiApiConsumer:
     
+    ''' Class to consumer swapi API with http resquests '''
+    
     def __init__(self) -> None:
         self.get_starships_response = namedtuple('GET_Starship', 'status_code request response')
     
     
-    def get_starships(self, page: int) -> any:
-        ''' Essa classe pode lidar com várias instâncias(requisições) '''
+    def get_starships(self, page: int) -> Tuple[int, Type[Request], Dict]:
+        '''
+        request starships in pagination
+        :parm - page: int with page of navegation 
+        :return - Tuple with status-code, request, response attrributes
+        
+        '''
           
            #TODO Essa classe pode lidar com várias instâncias. Talvez seja por isso que tenha o decoretor classmethod(um método da class em si)
             
@@ -29,7 +36,7 @@ class SwapiApiConsumer:
         # Em caso de sucesso
         if ((status_code >= 200) and (status_code <= 299)):
             return self.get_starships_response(
-                status_code= response.status_code, request = req, response = response.json()
+                status_code= status_code, request = req, response = response.json()
             )
         # Em caso de error retorno a excessão personalizada
         else:
--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -1,11 +1,10 @@
 from src.infra.swapi_api_consumer import SwapiApiConsumer
 from src.errors.http_request_error import HttpRequestError
 
-def test_get_starships():
-    ''' Criando teste do método associado as informações das naves '''
+def test_get_starships(requests_mock):
+    ''' Testing get_startships method '''
     
-    #requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json= {'requesicao':'sucesso'})
-    
+    requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json= {'requesicao':'sucesso', 'results' : [{}]})
     starship = SwapiApiConsumer()
     page = 1
     get_starships_response = starship.get_starships(page=page)
@@ -13,13 +12,17 @@ def test_get_starships():
     assert get_starships_response.request.method == 'GET'
     assert get_starships_response.request.url == 'https://swapi.dev/api/starships/'
     assert get_starships_response.request.params == {'page': page}
-    assert get_starships_response.request.status_code == 200
+    assert get_starships_response.status_code == 200
     
-    # TODO serve para saber se a instância pertence a uma determinada classe
-    assert isinstance(get_starships_response.request.response['results'], list) 
+    assert isinstance(get_starships_response.response['results'], list) 
+ 
+
+
+def test_get_starships_http_error(requests_mock): 
+    ''' Testing error in get_starships method'''
     
-''' 
-def test_get_starships_http_error():
+    requests_mock.get('https://swapi.dev/api/starships/', status_code=400, json= {'detail': 'something'})
+    
     starship = SwapiApiConsumer()
     page = 100
     try:
@@ -31,7 +34,7 @@ def test_get_starships_http_error():
         print(error.message)
         print(error.status_code)
     
-'''   
+
     
     
     
